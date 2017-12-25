dd3b189f8b362e04d86f8f48a7851ba30cf1ed7e
Haruka Matsumoto <matsumoto.haru@gmail.com>
2017-12-25 08:50:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ function deepestChild() {
     if (!elm.hasChildNodes()) {
       return elm;
     } else {
-      elm = elm.querySelector('*');
+      elm = document.querySelector('*');
     }
   }
   return elm;
